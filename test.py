721d3f4cd90b203fc1499fb2c0aa98b7a543d9fa
Gautham <>
2021-10-18 14:25:19 +0530

    first 3 exercises

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-# check two numbers and return true if one of the number is 100 or if the sum of the two numbers is 100
-def check_num_100():
+import os
+
+# Part 01
+
+# 1 - check two numbers and return true if one of the number is 100 or if the sum of the two numbers is 100
+def check_num_is_100():
     a = int(input("Enter a number: "))
     b = int(input("Enter another number: "))
 
@@ -8,3 +12,20 @@ def check_num_100():
     else:  
         print("False")
 
+# 2 - get the extension of a filename
+def file_extension():
+    file = input("Enter a filename: ")
+    ext = os.path.splitext(file)
+    print("File extension: " + ext[1])
+
+# 3 - replace every character in a given string with the character following it in the alphabet
+def replace_characters():
+    char = input("Enter a character: ")
+    new = []
+    for i in char:
+        uni = ord(i) + 1
+        if i == 'z':
+            new.append('a')
+        else:
+            new.append(chr(uni))
+    print(''.join(map(str,new)))
