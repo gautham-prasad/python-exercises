ca175c395c0b67337c12f180f044f02c9c20e08b
Gautham <>
2021-10-18 15:18:41 +0530

    added 1st question from 2nd part

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime 
 
 # Part 01
 
@@ -29,3 +29,32 @@ def replace_characters():
         else:
             new.append(chr(uni))
     print(''.join(map(str,new)))
+
+# 4 - get the current date, expected output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+def current_date():
+    print(datetime.datetime.now().strftime('%d-%m-%Y'))
+
+# 5 - create a new string adding "New!" in front of a given string
+# If the given string begins with "New!" already then return the original string
+
+def add_new():
+    string = input("Enter a string: ")
+    first = string.split()
+    if first[0] == "New!":
+        print(string)
+    else: 
+        print("New! " + string)
+
+
+# Part 02
+
+# 1 - create a new string from a given string taking the last 3 characters and added at both the front and back 
+# The string length must be 3 or more, if not, the original string is returned.
+
+def concat_string():
+    concat = input("Enter a string: ")
+    if len(concat) <= 3:
+        print(concat)
+    else:
+        new = concat[-3:]
+        print(new + concat + new)
