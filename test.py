a1d000565502caae46e53224573b1972ed44f5c6
Gautham <>
2021-10-19 23:42:55 +0530

    completed part 4 and workin on 5

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os, datetime 
-
+import random
 # Part 01
 
 # 1 - check two numbers and return true if one of the number is 100 or if the sum of the two numbers is 100
@@ -8,9 +8,9 @@ def check_num_is_100():
     b = int(input("Enter another number: "))
 
     if a == 100 or b == 100 or a + b == 100:
-        print("True")
+        return True
     else:  
-        print("False")
+        return False
 
 # 2 - get the extension of a filename
 def file_extension():
@@ -108,4 +108,131 @@ def check_specified():
     else: 
         print(check + " is not found")
 
-check_specified()
+# Part 03
+
+# 1 - find the number of even digits in a an array of integers
+def even_digits():
+    digits = input("Enter a number: ")
+    count = 0
+    for i in digits:
+        if int(i) % 2 == 0:
+            count += 1
+    print(count)
+
+# 2 - find the number of even values up to a given number
+def even_numbers_upto():
+    digit = input("Enter a number: ")
+    count = 0
+    for i in range(1,int(digit)):
+        if int(i) % 2 == 0:
+            count += 1
+    print(count)
+
+# 3 - check whether a given array of integers is sorted in ascending order
+def sort_check():    
+    integer = list(map(int,input("Enter an array of integers: ").strip().split()))
+    integer1 = sorted(integer)
+    if integer1 == integer: 
+        print("Sorted")
+    else: 
+        print("Not sorted")
+
+#  4 - get the largest even number from an array of integers.
+def sort_check():    
+    integer = list(map(int,input("Enter an array of integers: ").strip().split()))
+    j = 0
+    for i in integer:
+        if int(i) % 2 == 0 and i > j:
+            j = i
+        else: 
+            continue
+    print(j)
+
+# 5 - replace the first digit in a string (should contain at least one digit) with a $ character
+def replace_first_digit():
+    string = list(input("Enter a string with atleast one numeric character: "))
+    for i in string: 
+        if i.isdigit():
+            string[string.index(i)] = "$"
+            break
+        else: 
+            continue
+    print(''.join(map(str,string)))
+
+# Part 04
+
+# 1 - Given a year, report if it is a leap year
+def check_leap_year():
+    year = int(input("Enter a year: "))
+    if year % 4 != 0:
+        print('Not Leap year')
+    elif year % 100 == 0 and year % 400 != 0:
+        print("Not Leap year")
+    else: 
+        print("Leap year")
+
+# 2 - compare two objects to determine if the first one contains the same properties as the second one (which may also have additional properties) 
+# For example, objA and objB are equal (but not equal to objC)
+# const objA = { a: 1, b: 1, c: 1 }; 
+# const objB = { a: 1, b: 1, c: 1 }; 
+# const objC = { a: 1, b: 1, d: 1 };
+def compare_two_obj():
+    objA = { 'a': 1, 'b': 1, 'c': 1 }
+    objB = { 'a': 1, 'b': 1, 'c': 1 } 
+    objC = { 'a': 1, 'b': 1, 'd': 1 }
+    objects = [ objA, objB, objC]
+    
+    for i in range(0,len(objects)):
+        if i == len(objects) - 1:
+            if objects[0] == objects[-1]:
+                print("Objects are equal")
+            else:
+                print("Objects are not equal")
+        elif objects[i] == objects[i+1]:
+            print("Objects are equal")
+        else:
+            print("Objects are not equal")
+
+# 3 - convert a comma-separated values (CSV) string to a 2D array. 
+# A new line indicates a new row in the array. Example input:
+# abc,def,ghi 
+# jkl,mno,pqr
+# stu,vwx,yza
+def csv():
+    csv = list(input("Enter comma-separated values: ").split(','))
+    newcsv=[]
+    for i in csv:
+        if i != "\\n":
+            newcsv.append(i)
+        else:
+            print(newcsv)
+            newcsv = []
+    print(newcsv)
+
+# 4 - generate a random hexadecimal color code
+def hex_color():
+    chars = '0123456789ABCDEF'
+    print("Random Color: " + ''.join(random.sample(chars,6)))
+    
+# 5 - returns true if the provided predicate function returns true for all elements in a collection, false otherwise
+def return_true():
+    if check_num_is_100() is True:
+        print("True")
+    else:
+        print("False")
+
+# Part 05
+
+#  1 - return passed string with letters in alphabetical order. Example string: 'webmaster' Expected Output: 'abeemrstw'
+def alphabetical():
+    print("".join(sorted(input("Enter a string: "))))
+
+# 2 - accept a string as a parameter and counts the number of vowels within the string
+def vowels():
+    string = input("Enter a string: ")
+    vow = 'aeiou'
+    count = 0
+    for i in string:
+        if i in vow:
+            count += 1
+    print(count)
